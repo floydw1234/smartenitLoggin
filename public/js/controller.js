aa6da9baa128645e17a4132825d903945b87e450
floydw1234 <floydw1234@gmail.com>
2017-04-05 12:50:58 -0700

    works for now

--- public/js/controller.js
+++ public/js/controller.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp',[]);
 app.controller('mainController', ['$scope','$http','$q', function($scope, $http,$q){
 	$scope.daterange = '';
 	$scope.test = 1;
-	$scope.test2 = {};
+	$scope.test2 = [];
 	$scope.PostDataResponse = {};
 	$scope.ResponseDetails = {};
 	$scope.idList = {};
@@ -11,7 +11,7 @@ app.controller('mainController', ['$scope','$http','$q', function($scope, $http,
 	$scope.charts = [];
 	$scope.unique = [];
 	$scope.test1 = [];
-	$scope.dataPoints = [];
+	$scope.data = [];
 
 
 	$scope.buildList = function(postData){
@@ -26,53 +26,41 @@ app.controller('mainController', ['$scope','$http','$q', function($scope, $http,
 			}
 		return deferred.promise;
 	};
-	/*
-	var options =     {
-          "caption": "Tweet Sentiment vs Avg Air Pressure",
-          "subCaption": "IBM Bluemix Demo",
-          "xAxisName": "Tweet Sentiment Score ",
-          "yAxisName": "Avg Pressure(kPA) ",
-          "theme": "fint"
-         };
-	*/
 	$scope.buildCharts = function(types){
 		
-		var deferred = $q.defer();
-		var allOptions = [];
-		angular.forEach(types,function(type){
-			var options = [];
-			var option =     {
-          			"caption": "Power Usage In range: ",
-         			"subCaption": "Smartenit Data Demo",
-          			"xAxisName": "Time",
-          			"yAxisName": "KW",
-          			"theme": "fint"
-         		};
-			options.push(option);
-			data = [];
-			angular.forEach($scope.PostDataResponse,function(dataPoint){
-				if(dataPoint.type = type){
-					var obj = {
-						"label" : String(type) +" "+ String(dataPoint.datetime),
-						"value" : String(dataPoint.value)
+		var promises = [];
+		
+		$scope.unique.forEach(function(type){
+					var deferred = $q.defer();
+					$scope.prepareData(type);
+					var option =
+					{
+			  			title: type,
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
 					};
-					data.push(obj);
-				};
-			});
-			options.push(data);
-			options.push(type);
-			allOptions.push(options);
-			deferred.resolve(allOptions);
-			//if(type = "hourMedian"){$scope.test1 = options};
-			
+					deferred.resolve(option);
+					promises.push(deferred.promise);
 		});
-		return deferred.promise;
+		return $q.all(promises);
+	};
+	$scope.prepareData = function(type){
+		var deferred = $q.defer();
+		var promises = [];
+		//deferred.resolve(['timeStamp', type]);
+		//promises.push(deferred.promise);
+		$scope.PostDataResponse.forEach(function(response){
+			var deferred = $q.defer();
+			if ( type == response.type){
+				deferred.resolve([new Date(response.timeStamp*1000),response.value]);
+				promises.push(deferred.promise);
+			}
+		});
+		return $q.all(promises).then(function(data){
+			$scope.data.push(data);
+		});
 		
 	};
-	$scope.prepareData = function(data){
-		var deferred = $q.defer();
-		
-	}
 	$scope.uniqueArray = function(charts){
 		var unique = charts.filter(function(elem, index, self) {
    			return index == self.indexOf(elem);
@@ -100,16 +88,64 @@ app.controller('mainController', ['$scope','$http','$q', function($scope, $http,
 	
 	$scope.SendData = function () {
 		$scope.post().then(function(post){
+
 			return $scope.buildList(post);
 		}).then(function(types){
-			
+			//$scope.test1 = types;
 			return $scope.buildCharts(types);
 		}).then(function(options){
+
+				$scope.test1 = {
+			  			title: $scope.unique[3],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					};
+				$scope.test2 = [['timeStamp', $scope.unique[1]]].concat($scope.data[1]);
+				$scope.test3 = String($scope.unique[1]);
+
+				drawChart({
+			  			title: $scope.unique[0],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					},[['timeStamp', $scope.unique[0]]].concat($scope.data[0]),String($scope.unique[0]));
 				
-			options.forEach(function(option){
-				$scope.test1.push(option[1]);
-				//makeChart(option[0], option[1], option[2]);
-			});
+				drawChart({
+			  			title: $scope.unique[1],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					},[['timeStamp', $scope.unique[1]]].concat($scope.data[1]),$scope.unique[1]);
+				drawChart({
+			  			title: $scope.unique[2],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					},[['timeStamp', $scope.unique[2]]].concat($scope.data[2]),$scope.unique[2]);
+				drawChart({
+			  			title: $scope.unique[3],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					},[['timeStamp', $scope.unique[3]]].concat($scope.data[3]),$scope.unique[3]);
+				drawChart({
+			  			title: $scope.unique[4],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					},[['timeStamp', $scope.unique[4]]].concat($scope.data[4]),$scope.unique[4]);
+				drawChart({
+			  			title: $scope.unique[5],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					},[['timeStamp', $scope.unique[5]]].concat($scope.data[5]),$scope.unique[5]);
+				drawChart({
+			  			title: $scope.unique[6],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					},[['timeStamp', $scope.unique[6]]].concat($scope.data[6]),$scope.unique[6]);
+				drawChart({
+			  			title: $scope.unique[7],
+			  			curveType: 'function',
+			  			legend: { position: 'bottom' }
+					},[['timeStamp', $scope.unique[7]]].concat($scope.data[7]),$scope.unique[7]);
+						
+			
 		});		
         };
 
---
+++ public/js/googleCharts.js
@@ -0,0 +1,25 @@
+google.charts.load('current', {'packages':['corechart']});
+   //   google.charts.setOnLoadCallback(drawChart);
+	function drawChart(options, dataPoints,location) {
+        	var data = google.visualization.arrayToDataTable(dataPoints);
+
+        	var chart = new google.visualization.LineChart(document.getElementById(String(location)));
+
+       		chart.draw(data, options);
+      }
+/*
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Date', 'Sales'],
+          [new Date(1489698000000),  1000],
+          [new Date(1489699000000),  1170],
+          [new Date(1489700000000),  660],
+          [new Date(1409701000000),  1030]
+        ]);
+
+        var options = {
+          title: 'kW Power',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+*/
